d8a07a65f8c8e6eb69c449a192ff6c5a08985de4
Furkan Şenol <frkn.snl@hotmail.com>
2023-08-27 14:32:50 +0300

    update getAll url

--- backend/src/routes/eventRoute.ts
+++ backend/src/routes/eventRoute.ts
@@ -5,7 +5,7 @@ import { eventController } from '../controllers/eventController';
 const router = express.Router();
 //router.use(verifyToken);
 
-router.get('/', apiKeyMiddleware, eventController.getAllEvents);
+router.get('/all', apiKeyMiddleware, eventController.getAllEvents);
 router.post('/', eventController.createEvent)
 
 export default router;
