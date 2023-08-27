2426e4e622aa2abaf19c857fe7aa1c0a0f8d679e
Furkan Şenol <frkn.snl@hotmail.com>
2023-08-27 20:55:33 +0300

    add swagger for category

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,17 +3,24 @@ import express, { Request, Response } from 'express';
 import dotenv from 'dotenv'
 import categoryRouter from './routes/categoryRoute'
 import eventRouter from './routes/eventRoute'
+import swaggerUi from 'swagger-ui-express'
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerOptions from "./swagger/swaggerOptions";
+
 dotenv.config();
 
 connectionDB()
 
+const specs = swaggerJsdoc(swaggerOptions);
 const app = express()
 const port = process.env.PORT
 
 app.use(express.json())
 
 app.use('/api/categories', categoryRouter);
-app.use('/api/events',eventRouter)
+app.use('/api/events', eventRouter)
+
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 app.listen(port, () => {
     console.log(`API running, localhost:${port}`);
--- backend/src/routes/categoryRoute.ts
+++ backend/src/routes/categoryRoute.ts
@@ -5,7 +5,53 @@ import apiKeyMiddleware from '../utils/apiKeyController';
 const router = express.Router();
 //router.use(verifyToken);
 
+/** 
+* @swagger
+* /api/categories:
+*   get:
+*     summary: Tüm kategorileri getirir
+*     tags:
+*       - Category
+*     parameters:
+*       - in: query
+*         name: api_key
+*         schema:
+*           type: string
+*         required: true
+*         description: API anahtarı
+*     responses:
+*       '200':
+*         description: Başarılı yanıt
+*         content:
+*           application/json:
+*             schema:
+*               type: array
+*               items:
+*                 $ref: '#/components/schemas/Category'
+*/
 router.get('/', apiKeyMiddleware, categoryController.getAllCategories);
+
+/**
+ * @swagger
+ * /api/categories:
+ *   post:
+ *     summary: Yeni bir kategori oluştur
+ *     tags:
+ *       - Category
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Category'
+ *     responses:
+ *       '201':
+ *         description: Başarılı yanıt
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Category'
+ */
 router.post('/', categoryController.createCategory)
 
 export default router;
