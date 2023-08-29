e928029a1d5da30da3ad1b1c72920338d1d26fa8
Furkan Şenol <frkn.snl@hotmail.com>
2023-08-29 13:01:57 +0300

    add swagger from eventRoute

--- backend/src/routes/eventRoute.ts
+++ backend/src/routes/eventRoute.ts
@@ -5,7 +5,38 @@ import { eventController } from '../controllers/eventController';
 const router = express.Router();
 //router.use(verifyToken);
 
+/** 
+* @swagger
+* /api/events/all:
+*   get:
+*     summary: Tüm etkinlikleri getirir
+*     tags:
+*       - Event
+*     parameters:
+*       - in: query
+*         name: api_key
+*         schema:
+*           type: string
+*         required: true
+*         description: API anahtarı
+*       - in: query
+*         name: page
+*         schema:
+*           type: integer
+*         required: false
+*         description: Sayfa numarası (opsiyonel)
+*     responses:
+*       '200':
+*         description: Success
+*         content:
+*           application/json:
+*             schema:
+*               type: array
+*               items:
+*                 $ref: '#/components/schemas/Event'
+*/
 router.get('/all', apiKeyMiddleware, eventController.getAllEvents);
+
 router.post('/', eventController.createEvent)
 
 export default router;
