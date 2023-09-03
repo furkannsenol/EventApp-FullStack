82ef1f1909f14e11226c568fd9843d28ba21e069
Furkan Şenol <frkn.snl@hotmail.com>
2023-09-03 20:21:55 +0300

    update model data

--- frontend/src/model/categoryData.ts
+++ frontend/src/model/categoryData.ts
@@ -7,4 +7,5 @@ export interface ICategoryData {
 export interface ICategory {
     _id: string;
     name: string;
+    icon: string
 }
---
+++ frontend/src/model/eventData.ts
@@ -0,0 +1,32 @@
+export interface IPricing {
+    category_name: string;
+    pay: number;
+}
+
+export interface ILocation {
+    place: string;
+    city: string;
+    adress: string;
+    latitude: number;
+    longitude: number;
+}
+
+export interface IEvent {
+    _id: string
+    name: string;
+    description: string;
+    date: string;
+    show_time: string;
+    category: string;
+    images: string[];
+    owner: string;
+    location_information: ILocation;
+    pricing_type: string;
+    pricing_list: IPricing[];
+}
+
+export interface IEventData{
+    status:string
+    total_results:number
+    results:IEvent[]
+}
